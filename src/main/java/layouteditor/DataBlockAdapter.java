57a91e69c69604686ad04afc716e427d529d9b73
Legut <wlegutowski@gmail.com>
2020-04-24 15:38:02 +0200

    SR-2 LayoutEditor base has been designed. Following features has been implemented: adding new block, deleting block, moving block up and down, setting the title of each block, modifying block's type. Each Change is being stored in ArrayList to make it easier to export data.

---
+++ src/main/java/layouteditor/DataBlockAdapter.java
@@ -0,0 +1,108 @@
+package layouteditor;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.representation.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DataBlockAdapter extends ArrayAdapter<DataBlock> {
+    public DataBlockAdapter(Context context, ArrayList<DataBlock> dataBlocks) {
+        super(context, 0, dataBlocks);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        final DataBlock dataBlock = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.single_data_block_view, parent, false);
+        }
+
+        // Bind listener to deleteButton to make it work
+        final Button deleteButton = convertView.findViewById(R.id.delete_block_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LayoutEditor.dataBlocks.remove(position);
+                LayoutEditor.dataBlockAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // Bind listener to moveDownButton to make it work
+        final Button moveDownButton = convertView.findViewById(R.id.move_block_down_button);
+        moveDownButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(LayoutEditor.dataBlocks.size()>position+1) {
+                    Collections.swap(LayoutEditor.dataBlocks, position, position + 1);
+                    LayoutEditor.dataBlockAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        // Bind listener to moveUpButton to make it work
+        final Button moveUpButton = convertView.findViewById(R.id.move_block_up_button);
+        moveUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(position>0) {
+                    Collections.swap(LayoutEditor.dataBlocks, position, position - 1);
+                    LayoutEditor.dataBlockAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        // Configure EditText's TextWatcher. Create new ViewHolder and apply EditText to it.
+        EditText blockTitleInput = convertView.findViewById(R.id.block_title_input);
+        ViewHolder holder = new ViewHolder();
+        holder.editText = blockTitleInput;
+
+        // If EditText already have a TextWatcher then remove it
+        TextWatcher oldWatcher = (TextWatcher) holder.editText.getTag();
+        if(oldWatcher != null)
+            holder.editText.removeTextChangedListener(oldWatcher);
+
+        // Apply data (from ArrayList connected to the adapter) to EditText
+        blockTitleInput.setText(dataBlock.getBlockTitle());
+
+        // Declare new TextWatcher and put it in editText tag. Also bind it to this EditText
+        CustomTextWatcher newWatcher = new CustomTextWatcher(position);
+        holder.editText.setTag(newWatcher);
+        holder.editText.addTextChangedListener(newWatcher);
+
+        // Handle block type spinner. Set values provided to adapter by ArrayList
+        Spinner blockTypeInput = convertView.findViewById(R.id.block_type_spinner);
+        switch(dataBlock.getBlockType()){
+            case VALUE:
+                blockTypeInput.setSelection(0);
+                break;
+            case TABLE:
+                blockTypeInput.setSelection(1);
+                break;
+            case CHART:
+                blockTypeInput.setSelection(2);
+                break;
+        }
+        // Apply custom OnItemSelectedListener, which will change date in ArrayList connected witch Adapter,
+        // when user apply changes to the spinner on the ListView
+        blockTypeInput.setOnItemSelectedListener(new CustomOnItemSelectedListener(position));
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
+    static class ViewHolder {
+        EditText editText;
+    }
+}
+
---
+++ src/main/java/layouteditor/LayoutEditor.java
@@ -0,0 +1,93 @@
+package layouteditor;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.representation.R;
+
+import java.util.ArrayList;
+
+public class LayoutEditor extends AppCompatActivity {
+    public static ArrayList<DataBlock> dataBlocks;
+    public static DataBlockAdapter dataBlockAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_editor);
+
+        // Configure action bar
+        Toolbar actionbar = findViewById(R.id.action_bar);
+        actionbar.setTitle(R.string.LAYOUT_EDITOR_ACTION_BAR_TITLE);
+        setSupportActionBar(actionbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        // Construct the data source
+        dataBlocks = new ArrayList<>();
+        dataBlocks.add(new DataBlock("Przykładowy tytuł", DataBlock.BlockTypeEnum.CHART));
+        dataBlocks.add(new DataBlock("Drugi tytuł", DataBlock.BlockTypeEnum.VALUE));
+        dataBlocks.add(new DataBlock("Trzeci tytuł", DataBlock.BlockTypeEnum.TABLE));
+
+        // Create the adapter to convert the array to views
+        dataBlockAdapter = new DataBlockAdapter(this, dataBlocks);
+
+        // Attach the adapter to a ListView
+        ListView list = findViewById(R.id.layout_editor_list_view);
+        list.setAdapter(dataBlockAdapter);
+
+        // Define buttons actions
+        final Button addButton = findViewById(R.id.add_block_to_layout);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dataBlocks.add(new DataBlock());
+                dataBlockAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.save_layout_in_storage) {
+            Toast.makeText(getApplicationContext(), "Zapisz układ na dysku", Toast.LENGTH_SHORT).show();
+            // TODO: Functionality of saving layouts to the xml (or another extension) file in device storage
+        } else if (itemId == R.id.load_layout_from_file) {
+            Toast.makeText(getApplicationContext(), "Wczytaj układ z pliku", Toast.LENGTH_SHORT).show();
+            // TODO: Functionality of loading files from file to the app
+        } else if (itemId == R.id.save_Layout_in_app) {
+            Toast.makeText(getApplicationContext(), "Zapisz układ", Toast.LENGTH_SHORT).show();
+            // TODO: Functionality od saving layouts inside the app
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.layout_editor_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        // TODO: define what should happen when you click back button
+        Log.println(Log.INFO,"LayoutEditor: ", "Back Button Pressed");
+    }
+}

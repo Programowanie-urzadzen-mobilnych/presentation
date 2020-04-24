cba4d8db64ee4fb14f2e6ad70dc0ef97ced3ccb4
Paweł Osiwała <pawelosiwala@gmail.com>
2020-04-24 18:41:42 +0200

    User can choose where he wants to save exported data Testing PDF creator

--- src/main/java/SaveData/SaveData.java
+++ src/main/java/SaveData/SaveData.java
@@ -1,23 +1,15 @@
 package SaveData;
 
 import android.Manifest;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.ContentUris;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.pdf.PdfDocument;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -28,14 +20,13 @@ import com.representation.R;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.file.Path;
+import java.util.ArrayList;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import lib.folderpicker.FolderPicker;
 
 /*
 	Export results to a PDF / XML file with an ability to choose save location#41
@@ -48,7 +39,9 @@ import androidx.core.content.ContextCompat;
 public class SaveData extends Activity {
 
     private static final int WRITE_EXTERNAL_STORAGE_STATUS = 0;
-    String dane = "Losowe dane mile widziane";
+    private static final int FOLDERPICKER_CODE = 9998;
+
+    ArrayList<ExportData> exportDataArrayList = new ArrayList<ExportData>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,26 +49,50 @@ public class SaveData extends Activity {
         setContentView(R.layout.activity_save_data);
 
         //TODO: unpack data from bundle
+        //lets say we are getting string with variable names separated by ';'
+        Intent intent = getIntent();
+        String variableNames = intent.getStringExtra("variableNames");
+        //test string
+        variableNames = "Temperatura;Cisnienie;Wilgotnosc;Napiecie";
+
+        String names[] = variableNames.split(";");
+
+        Log.i("OnCreate","Start "+names.length);
+        //now lets unload those variables
+        for (String name: names) {
+
+            ExportData tempExportData = new ExportData(name);
+
+            //String values = intent.getStringExtra(name);
+            //String timeStamps = intent.getStringExtra(name+"_timeStamps");
+
+            //test strings;
+            String values =        "1.23;      2.03;       3.33;       4.20;       6.12;       5.66;       4.44;       3.22";
+            String timeStamps =    "06:43:19;  08:22:21;   09:42:43;   10:25:19;   11:13:43;   13:33:19;   15:43:19;   18:43:19";
+
+            String[] tempV = values.split(";");
+            String[] tempT = timeStamps.split(";");
+
+            if(tempV.length!=tempT.length){
+                //throw new MissingDataException;
+                Log.i("error","dane są nie równe");
+            }else{
+                for (int i = 0; i < tempV.length-1; i++) {
+                    tempExportData.addValue(tempV[i],tempT[i]);
+                    Log.i("ExportData: ",tempV[i]+" "+tempT[i]);
+                }
+            }
+
+            exportDataArrayList.add(tempExportData);
+        }
 
         final Button button = findViewById(R.id.ZapiszDaneDoPDFButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                saveDataToPDF();
+                Intent intent = new Intent(getApplicationContext(), FolderPicker.class);
+                startActivityForResult(intent, FOLDERPICKER_CODE);
             }
         });
-    }
-
-    private void saveDataToPDF(){
-        selectFilePath();
-    }
-
-    private void selectFilePath() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-            i.addCategory(Intent.CATEGORY_DEFAULT);
-            startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999);
-        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -83,137 +100,19 @@ public class SaveData extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch(requestCode) {
-            case 9999:
-                if(data!=null){
-                    Log.i("Test", "Result URI " + data.getData());
-
-                    Uri uri = data.getData();
-                    Uri docUri = DocumentsContract.buildDocumentUriUsingTree(uri,
-                            DocumentsContract.getTreeDocumentId(uri));
-                    String filePath = getPath(this, docUri);
-
-
-                    Log.i("Test", "Result Path " + filePath);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                        //stringtopdf(filePath);
-                        createFile(filePath);
-                    }
-                }else{
-                    Log.i("error","Failed to provide with URI - no data");
+        if(resultCode != RESULT_CANCELED){
+            if(data!= null){
+                if (requestCode == FOLDERPICKER_CODE && resultCode == Activity.RESULT_OK) {
+                    String folderLocation = data.getExtras().getString("data");
+                    Log.i( "folderLocation", folderLocation );
+                    createFile(folderLocation);
                 }
-                break;
-            default:
-                Log.i("error","Failed to provide with URI - ?");
-                break;
-        }
-
-    }
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    public static String getPath(final Context context, final Uri uri) {
-
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-
-        // DocumentProvider
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            // ExternalStorageProvider
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-
-                // TODO handle non-primary volumes
+            }else{
+                Log.i("error","Failed to provide with URI - no data");
             }
-            // DownloadsProvider
-            else if (isDownloadsDocument(uri)) {
-
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[] {
-                        split[1]
-                };
-
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-            }
+        }else{
+            Log.i("error","Request cancelled");
         }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
-
-            // Return the remote address
-            if (isGooglePhotosUri(uri))
-                return uri.getLastPathSegment();
-
-            return getDataColumn(context, uri, null, null);
-        }
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
-        }
-
-        return null;
-    }
-
-    public static String getDataColumn(Context context, Uri uri, String selection,String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {
-                column
-        };
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
-        }
-        return null;
-    }
-
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    public static boolean isGooglePhotosUri(Uri uri) {
-        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -237,56 +136,60 @@ public class SaveData extends Activity {
         String temp = String.valueOf(WRITE_EXTERNAL_STORAGE_STATUS);
         if(WRITE_EXTERNAL_STORAGE_STATUS==0){
             Log.i("Test","Write external storage status: "+temp);
-            createPdf(dane,filePath);
-/*            File path  = new File(filePath);
-            String fileName = "myfile.txt";
-            String value = "BENIZ";
-
-            File output = new File(filePath,"myfile.txt");
-
-            try {
-                FileOutputStream fileout = new FileOutputStream(output.getAbsolutePath());
-                OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
-                outputWriter.write(value);
-                outputWriter.close();
-                //display file saved message
-                Log.i("Success","File saved in "+filePath);
-                Toast.makeText(getBaseContext(), "File saved successfully!",
-                        Toast.LENGTH_SHORT).show();
-            }
-            catch (IOException e) {
-                Log.e("Exception", "File write failed: " + e.toString());
-            }*/
+            createPdf(filePath);
         }else{
             //createFile(filePath); //nie jestem pewny czy to bezpieczne będzie :D
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void createPdf(String data,String path){
+    private void createPdf(String path){
+
+        int pageWidth = 300;
+        int pageHeight = 600;
+
         // create a new document
         PdfDocument document = new PdfDocument();
         // crate a page description
-        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(300, 600, 1).create();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, 1).create();
         // start a page
         PdfDocument.Page page = document.startPage(pageInfo);
+
+        //prepare paint
         Canvas canvas = page.getCanvas();
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        canvas.drawCircle(50, 50, 30, paint);
         paint.setColor(Color.BLACK);
-        canvas.drawText(data, 80, 50, paint);
-        //canvas.drawt
-        // finish the page
-        document.finishPage(page);
-        // draw text on the graphics object of the page
-        // Create Page 2
-        pageInfo = new PdfDocument.PageInfo.Builder(300, 600, 2).create();
-        page = document.startPage(pageInfo);
-        canvas = page.getCanvas();
-        paint = new Paint();
-        paint.setColor(Color.BLUE);
-        canvas.drawCircle(100, 100, 100, paint);
+
+        //write data
+        //canvas.drawText(data, 80, 50, paint);
+
+        float x=0,y=50;
+
+        //TODO: if y > pageheight + offset -> nextpage
+
+        canvas.drawText("Export danych",60,y,paint);
+        y+=30;
+
+        for (ExportData exportData: exportDataArrayList) {
+
+            //subtitle
+            String subtitle = exportData.getValuesName();
+            canvas.drawText(subtitle,40,y,paint);
+            y+=30;
+
+            //values as value | timestamp
+            String workingString = "";
+            Log.i("PDFCreator name: ",subtitle);
+            Log.i("PDFCreator dataSize: ",String.valueOf(exportData.getDataLength()));
+            for(int a = 0; a < exportData.getDataLength()-1; a++){
+                workingString = exportData.getStringOf(a);
+                Log.i("PDFCreator string: ",workingString);
+                canvas.drawText(workingString,20,y,paint);
+                y+=20;
+            }
+        }
+
+
         document.finishPage(page);
 
         // write the document content
@@ -306,31 +209,5 @@ public class SaveData extends Activity {
         }
         // close the document
         document.close();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public void stringtopdf(String filePath) {
-
-        String extstoragedir = filePath;
-
-        File dir = new File(extstoragedir, "a directory");
-        if(!dir.exists()){
-            // do something
-            Log.i("Success","Utworzono folder w "+extstoragedir+"/a directory");
-        }
-        File file = new File(extstoragedir + "/a directory/" + "a file");
-        if(!file.exists()){
-            Log.i("Success","Utworzono plik w "+extstoragedir + "/a directory/" + "a file");
-
-        }
-
-        try {
-
-            Log.i("Success","Zapisano plik w "+extstoragedir);
-
-        } catch (Exception e) {
-            Log.i("error", e.getLocalizedMessage());
-            Log.i("error","Nie można zapisać pliku w "+extstoragedir);
-        }
     }
 }

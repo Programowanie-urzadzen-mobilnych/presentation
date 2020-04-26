1d4c9e2c60d16a06654b4d2a656a26984fe36b93
Legut <wlegutowski@gmail.com>
2020-04-26 16:53:48 +0200

    SR-6 Added empty activity

--- src/main/java/com/representation/MainActivity.java
+++ src/main/java/com/representation/MainActivity.java
@@ -7,13 +7,14 @@ import android.os.Bundle;
 
 import layouteditor.LayoutEditor;
 import layouts.LayoutsList;
+import measurements.Measurements;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent i = new Intent(this, LayoutsList.class);
+        Intent i = new Intent(this, Measurements.class);
         startActivity(i);
     }
 }
---
+++ src/main/java/measurements/Measurements.java
@@ -0,0 +1,16 @@
+package measurements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.representation.R;
+
+public class Measurements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurements);
+    }
+}

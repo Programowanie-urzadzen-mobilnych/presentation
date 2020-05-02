4aab43a7bde722b02e1f3c503c69925bdb1286cb
Sylwester Wrzesinski <sw-afk>
2020-05-02 16:42:17 +0200

    SR-36 Added charts

--- src/main/java/com/representation/MainActivity.java
+++ src/main/java/com/representation/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import charts.TestActivity;
 import data.Database;
 import measurements.Measurements;
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent i = new Intent(this, Measurements.class);
+        Intent i = new Intent(this, TestActivity.class);
         startActivity(i);
     }
 }

df9014a8f901658ede8df569fe90c7f184f57dee
Legut <wlegutowski@gmail.com>
2020-05-10 21:24:41 +0200

    SR-36 Some small changes simplifying upcoming merge.

--- src/main/java/com/representation/MainActivity.java
+++ src/main/java/com/representation/MainActivity.java
@@ -1,19 +1,17 @@
 package com.representation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 
-import charts.TestActivity;
-import data.Database;
+import androidx.appcompat.app.AppCompatActivity;
+
 import measurements.Measurements;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent i = new Intent(this, TestActivity.class);
+        Intent i = new Intent(this, Measurements.class);
         startActivity(i);
     }
 }
